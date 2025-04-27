e2fd87f7e228428e3b6b544fff1807250fe2921d
JohnArbaiza1 <johnarbaiza07.03@gmail.com>
2025-04-27 00:11:19 -0600

    Update: info planes

--- database/seeders/MembershipsTableSeeder.php
+++ database/seeders/MembershipsTableSeeder.php
@@ -16,7 +16,7 @@ class MembershipsTableSeeder extends Seeder
         DB::table('tbl_memberships')->insert([
             // Plan Inicial
             [
-                'membership_name' => 'Inicial',
+                'membership_name' => 'Debug',
                 'price' => 0.00,
                 'membership_description' => 'Postulaciones limitadas para que empieces de forma sencilla. Acceso básico a las funcionalidades esenciales para explorar y conocer la plataforma.',
                 'unlimited_applications' => false,
@@ -25,16 +25,16 @@ class MembershipsTableSeeder extends Seeder
             ],
             // Plan Pro
             [
-                'membership_name' => 'Pro',
+                'membership_name' => 'DeployPro',
                 'price' => 10.00,
-                'membership_description' => 'Amplía tus posibilidades con 8 postulaciones mensuales. Conéctate y colabora en hasta 4 Proyectos Múltiples. Publicación de hasta 3 proyectos. Destácate entre la multitud con mayor visibilidad en la plataforma.',
+                'membership_description' => 'Amplía tus posibilidades con aplicaciones ilimitadas a proyectos. Diseñado para quienes quieren aprovechar al máximo las oportunidades disponibles en la plataforma.',
                 'unlimited_applications' => false,
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             // Plan Inicial Enterprise
             [
-                'membership_name' => 'Inicial Enterprise',
+                'membership_name' => 'Debug Enterprise',
                 'price' => 0.00,
                 'membership_description' => 'Publica 1 proyecto y atrae al profesional ideal. Evalúa hasta 2 candidatos para elegir al mejor. Visibilidad básica para tu proyecto en la plataforma.',
                 'unlimited_applications' => false,
@@ -45,16 +45,16 @@ class MembershipsTableSeeder extends Seeder
             [
                 'membership_name' => 'Pro Enterprise',
                 'price' => 20.00,
-                'membership_description' => 'Publica 5 proyectos al mes y amplía tus opciones. Evalúa hasta 8 candidatos para elegir al mejor. Visibilidad destacada para atraer talento top.',
+                'membership_description' => 'Publicaciones ilimitadas de proyectos. Pensado para empresas que buscan encontrar talento constantemente y sin límites.',
                 'unlimited_applications' => false,
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
-            // Plan Todo en Uno
+            // Plan que tiene Todo en Uno
             [
-                'membership_name' => 'Todo en Uno',
+                'membership_name' => 'TechStack Max',
                 'price' => 28.00,
-                'membership_description' => 'Postulaciones y Publicaciones Ilimitadas. Expande tu red y colabora en Múltiples Proyectos. Encuentra al talento perfecto con Evaluaciones Ilimitadas. Visibilidad Máxima. Descubre lo último primero con Acceso Exclusivo Anticipado.',
+                'membership_description' => 'Publica y aplica sin límites. Expande tu red y colabora en Múltiples Proyectos. Encuentra al talento perfecto de manera Ilimitadas. Visibilidad Máxima. Descubre lo último primero con Acceso Exclusivo Anticipado.',
                 'unlimited_applications' => true,
                 'created_at' => now(),
                 'updated_at' => now(),
